191e899809acba14759a4a8225a4eddf2ef86633
Alex Kozak <a.kozak1@outlook.com>
2019-04-18 03:10:32 -0400

    Workstation Andon implimented, flag for when stock is low needs to be added. Possibly red indicator.

---
+++ AK_KANBAN_Andon/AK_KANBAN_Config/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace AK_KANBAN_Config
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.dgvConfig = new System.Windows.Forms.DataGridView();
+            this.cbWorkstation = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbAutoRefresh = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConfig)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Enabled = false;
+            this.btnConnect.Location = new System.Drawing.Point(311, 306);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // dgvConfig
+            // 
+            this.dgvConfig.AllowUserToAddRows = false;
+            this.dgvConfig.AllowUserToDeleteRows = false;
+            this.dgvConfig.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConfig.Location = new System.Drawing.Point(12, 50);
+            this.dgvConfig.Name = "dgvConfig";
+            this.dgvConfig.ReadOnly = true;
+            this.dgvConfig.Size = new System.Drawing.Size(374, 250);
+            this.dgvConfig.TabIndex = 4;
+            // 
+            // cbWorkstation
+            // 
+            this.cbWorkstation.FormattingEnabled = true;
+            this.cbWorkstation.Location = new System.Drawing.Point(11, 24);
+            this.cbWorkstation.Margin = new System.Windows.Forms.Padding(2);
+            this.cbWorkstation.Name = "cbWorkstation";
+            this.cbWorkstation.Size = new System.Drawing.Size(92, 21);
+            this.cbWorkstation.TabIndex = 9;
+            this.cbWorkstation.SelectedValueChanged += new System.EventHandler(this.CbWorkstation_SelectedValueChanged);
+            this.cbWorkstation.Click += new System.EventHandler(this.CbWorkstation_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Workstation";
+            // 
+            // cbAutoRefresh
+            // 
+            this.cbAutoRefresh.AutoSize = true;
+            this.cbAutoRefresh.Enabled = false;
+            this.cbAutoRefresh.Location = new System.Drawing.Point(11, 312);
+            this.cbAutoRefresh.Name = "cbAutoRefresh";
+            this.cbAutoRefresh.Size = new System.Drawing.Size(86, 17);
+            this.cbAutoRefresh.TabIndex = 11;
+            this.cbAutoRefresh.Text = "Auto-Update";
+            this.cbAutoRefresh.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 339);
+            this.Controls.Add(this.cbAutoRefresh);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbWorkstation);
+            this.Controls.Add(this.dgvConfig);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "frmMain";
+            this.Text = "AK_KANBAN Config";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConfig)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.DataGridView dgvConfig;
+        private System.Windows.Forms.ComboBox cbWorkstation;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbAutoRefresh;
+    }
+}
+
---
+++ AK_KANBAN_Andon/AK_KANBAN_Config/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace AK_KANBAN_Config
+{
+    public partial class frmMain : Form
+    {
+        System.Timers.Timer timer = new System.Timers.Timer();
+        private delegate void ThreadSafeDelegate();
+
+        private string connectionString = ConfigurationManager.AppSettings["dbConnString"].ToString();
+
+        private BindingSource bindingConfig = new BindingSource();
+        private SqlDataAdapter dataAdapterConfig;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            timer.Elapsed += refreshBins;
+            timer.Interval = 1000;
+            timer.Start();
+            dgvConfig.DataSource = bindingConfig;
+
+            dataAdapterConfig = new SqlDataAdapter("Select * FROM tblConfig", connectionString);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            refreshBins();
+        }
+
+        void refreshBins()
+        {
+            GetData(string.Format("Select PartName, BinSize, BinLevel FROM tblPartBins WHERE WorkStationID = {0}", cbWorkstation.SelectedItem.ToString()), dataAdapterConfig, bindingConfig);
+        }
+
+        private void refreshBins(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                if (cbAutoRefresh.Checked)
+                {
+                    Invoke(new ThreadSafeDelegate(refreshBins));
+                }
+            }
+            catch { }
+        }
+
+        private void GetData(string selectCommand, SqlDataAdapter sda, BindingSource bs)
+        {
+            try
+            {
+                // Create a new data adapter based on the specified query.
+                sda = new SqlDataAdapter(selectCommand, connectionString);
+
+                // Create a command builder to generate SQL update, insert, and
+                // delete commands based on selectCommand. 
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(sda);
+
+                // Populate a new data table and bind it to the BindingSource.
+                DataTable table = new DataTable
+                {
+                    Locale = CultureInfo.InvariantCulture
+                };
+                sda.Fill(table);
+                bs.DataSource = table;
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("To run this example, replace the value of the " +
+                    "connectionString variable with a connection string that is " +
+                    "valid for your system.");
+            }
+        }
+
+        private void CbWorkstation_Click(object sender, EventArgs e)
+        {
+            cbWorkstation.Items.Clear();
+            foreach (DataRow dr in GetData("SELECT ID FROM tblWorkStations WHERE IsActive = 1", dataAdapterConfig).Rows)
+            {
+                cbWorkstation.Items.Add(dr[0]);
+            }
+        }
+
+        private DataTable GetData(string selectCommand, SqlDataAdapter sda)
+        {
+            DataTable table = new DataTable
+            {
+                Locale = CultureInfo.InvariantCulture
+            };
+            try
+            {
+                // Create a new data adapter based on the specified query.
+                sda = new SqlDataAdapter(selectCommand, connectionString);
+
+                // Create a command builder to generate SQL update, insert, and
+                // delete commands based on selectCommand. 
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(sda);
+
+                // Populate a new data table and bind it to the BindingSource.
+
+                sda.Fill(table);
+
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("To run this example, replace the value of the " +
+                    "connectionString variable with a connection string that is " +
+                    "valid for your system.");
+            }
+            return table;
+        }
+
+        private void CbWorkstation_SelectedValueChanged(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = cbWorkstation.SelectedItem != null;
+            cbAutoRefresh.Enabled = cbWorkstation.SelectedItem != null;
+        }
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer.Stop();
+        }
+    }
+}
